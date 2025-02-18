9f52c2c71065c27efc7542d9e88b9092bd041958
moekyi <khinmoekyi.mmt@gmail.com>
2025-02-18 15:25:47 +0630

    Add files via upload

---
+++ login1.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+# Function to handle login action 
+def login_action():
+    username = username_entry.get()
+    password = password_entry.get()
+    if username == "" or password == "":
+        result_label.config(text="Please enter both username and password.", foreground="red")
+    elif username != 'mmt' or password != '123':
+        messagebox.showerror("Error")
+    else:
+        messagebox.showinfo("Welcome")
+        win.destroy()
+        import main_sys
+# Function to handle the "Cancel" button click
+def cancel_action():
+    username_entry.delete(0, tk.END)  # Clear username entry
+    password_entry.delete(0, tk.END)  # Clear password entry
+    result_label.config(text="", foreground="black")  # Clear result message
+    
+
+# Create the main window
+win = tk.Tk()
+win.geometry("400x250")
+win.title('Login Form')
+# Create a LabelFrame to contain the login form
+login_frame = tk.Frame(win)
+login_frame.grid(row=0, column=0, padx=20, pady=20)
+# Creating a label for Username
+username_label = ttk.Label(login_frame, text="Username")
+username_label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+# Creating an entry for Username
+username_entry = ttk.Entry(login_frame)
+username_entry.grid(row=0, column=1, padx=10, pady=10)
+# Creating a label for Password
+password_label = ttk.Label(login_frame, text="Password")
+password_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+# Creating an entry for Password
+password_entry = ttk.Entry(login_frame, show="*")
+password_entry.grid(row=1, column=1, padx=10, pady=10)
+# Creating a result label to display messages
+result_label = ttk.Label(login_frame, text="", foreground="black")
+result_label.grid(row=2, columnspan=2, pady=10)
+# Creating a login button
+login_button = ttk.Button(login_frame, text="Login", command=login_action)
+login_button.grid(row=3, column=0, padx=10, pady=20)
+# Creating a "Cancel" button and bind it to cancel_action()
+cancel_button = ttk.Button(login_frame, text="Reset/Clear", command=cancel_action)
+cancel_button.grid(row=3, column=1, padx=10, pady=10)
+
+# Start the Tkinter event loop
+win.mainloop()
+
+
---
+++ main_sys.py
@@ -0,0 +1,146 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+
+    # Create the main application window
+root = tk.Tk()
+root.title("Travel System ")
+    #root.geometry("800x300")
+    # Create a Treeview widget
+tree = ttk.Treeview(root, columns=("Zone", "Station"), show="headings")
+tree.heading("Zone", text="Zone")
+tree.heading("Station", text="Station")
+tree.column('Station', width=600)
+
+    # Add sample data
+tree.insert("", "end", values=("1", "Yaen, Jaund, Tallan, Rede, Ninia, Bylyn, Frestin, Lomil, Soth"))
+tree.insert("", "end", values=("2", "Agralle, Docia, Stonyam, Obelyn, talith, Garion, Sylas, Riclya, Quthief,Wicyt, Riladia, Oloadus"))
+tree.insert("", "end", values=("3", "Zord, Perinad, Keivia,Erean, Brunad, Marend, Ryall, Pryn, Edenif, Holmer, Vertwall, Runil, Elyot, Adohad"))
+    #tree.pack(pady=10,padx=10)
+tree.grid(row=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+    # Create a LabelFrame to contain the system
+sys_frame = ttk.LabelFrame(root, text="System Operations", padding="10")
+    #sys_frame.pack(side='left',padx=20, pady=20)
+sys_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+    # Create a LabelFrame for Voucher Operations
+voucher_frame = ttk.LabelFrame(root, text="Generate Voucher", padding="10")
+    #voucher_frame.pack(side='center',padx=20, pady=20)
+voucher_frame.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
+
+    # Define button callback functions
+def add_item():
+        # messagebox.showinfo("Add", "Add button clicked")
+    add_window = tk.Toplevel(root)  # Create a new popup window
+    add_window.title("Add New Zone and Station")
+    add_window.geometry("300x200")
+
+    # Labels and Entry Fields
+    tk.Label(add_window, text="Zone:").grid(row=0, column=0, padx=10, pady=5)
+    entry_zone = tk.Entry(add_window, width=20)
+    entry_zone.grid(row=0, column=1, padx=10, pady=5)
+
+    tk.Label(add_window, text="Station").grid(row=1, column=0, padx=10, pady=5)
+    entry_station = tk.Entry(add_window, width=20)
+    entry_station.grid(row=1, column=1, padx=10, pady=5)
+
+        
+    def add_newitem():
+        zone = entry_zone.get()
+        station = entry_station.get()
+     
+    # Check if both fields are filled
+        if not zone or not station:
+            result_label.config(text="All fields must be filled!.", foreground="red")
+            return
+        
+    # Iterate through existing items to check for duplicates
+        for item in tree.get_children():
+            item_values = tree.item(item, 'values')
+            if  item_values[0] == zone or item_values[1] == station:
+                messagebox.showerror("Error", "This zone is already exists!")
+                return   
+
+    # Insert the new item
+        tree.insert("", "end", values=(zone, station))
+        add_window.destroy()  # Close the popup window after adding
+    
+    # Creating a result label to display messages
+    result_label = ttk.Label(add_window, text="", foreground="black")
+    result_label.grid(row=2, columnspan=2, pady=10)
+    
+    # Add button inside popup window
+    btn_submit = tk.Button(add_window, text="Add", command=add_newitem)
+    btn_submit.grid(row=3, columnspan=2, pady=10, padx=10)
+    
+    def clear_action():
+        entry_zone.delete(0, tk.END)  # Clear zone entry
+        entry_station.delete(0, tk.END)  # Clear station entry
+        result_label.config(text="", foreground="black")  # Clear result message
+    
+    #
+        # reset  button inside popup window
+    btn_cancel = tk.Button(add_window, text="Reset", command=clear_action)
+    btn_cancel.grid(row=3, column=1, pady=10, padx=10)
+
+
+
+def edit_item():
+    selected_item = tree.selection()
+    if not selected_item:
+        messagebox.showwarning("Warning", "Please select an item to edit!")
+        return
+    # Get selected item data
+    item = selected_item[0]
+    values = tree.item(item, "values")
+    # Create an edit popup window
+    edit_window = tk.Toplevel(root)  # Create a new popup window
+    edit_window.title("Add New Zone and Station")
+    edit_window.geometry("300x200")
+
+    # Labels and Entry Fields
+    tk.Label(edit_window, text="Zone:").grid(row=0, column=0, padx=10, pady=5)
+    entry_zone = tk.Entry(edit_window, width=20)
+    entry_zone.grid(row=0, column=1, padx=10, pady=5)
+
+    tk.Label(edit_window, text="Station").grid(row=1, column=0, padx=10, pady=5)
+    entry_station = tk.Entry(edit_window, width=20)
+    entry_station.grid(row=1, column=1, padx=10, pady=5)  
+    def edit_newitem():
+         pass
+    # Add button inside edit-popup window
+    def cancel_newitem():
+       pass 
+    
+    btn_edit = tk.Button(edit_window, text="Save", command=edit_newitem)
+    btn_edit.grid(row=2, column=0, pady=10, padx=10)
+    btn_cancel = tk.Button(edit_window, text="Cancel", command=cancel_newitem)
+    btn_edit.grid(row=2, column=1, pady=10, padx=10)
+
+
+def delete_item():
+        messagebox.showinfo("Delete", "Delete button clicked")
+    # Define the voucher button callback function
+def voucher_operation():
+    root.destroy()
+    import Gen_voucher
+    Gen_voucher.generate_voucher()
+    
+    # Create buttons and add them to the LabelFrame
+add_button = ttk.Button(sys_frame, text="Add", command=add_item)
+edit_button = ttk.Button(sys_frame, text="Edit", command=edit_item)
+delete_button = ttk.Button(sys_frame, text="Delete", command=delete_item)
+
+
+    # Pack buttons into the LabelFrame
+    # add_button.pack(side="left", padx=5, pady=5)
+    # edit_button.pack(side="left", padx=5, pady=5)
+    # delete_button.pack(side="left", padx=5, pady=5)
+add_button.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+edit_button.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
+delete_button.grid(row=1, column=2, padx=10, pady=10, sticky="nsew")
+    # Create the Voucher button and add it to the Voucher Operations LabelFrame
+voucher_button = ttk.Button(voucher_frame, text="Voucher", command=voucher_operation)
+    #voucher_button.pack(side="left",padx=5, pady=5)
+voucher_button.grid(row=1, column=3, padx=10, pady=10, sticky="nsew")
+root.mainloop()
+    # return
